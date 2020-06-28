3d8473c68c9de55282209bb0edb0fbb38522e6a9
726706687@qq.com <726706687@qq.com>
2020-06-28 15:15:09 +0800

    注解

--- Chapter5/src/main/java/com/course/testng/basicannation.java
+++ Chapter5/src/main/java/com/course/testng/basicannation.java
@@ -1,10 +1,24 @@
 package com.course.testng;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class basicannation {
     @Test
     public void testCase1(){
         System.out.println("这是测试用例一");
+    }
+    @Test
+    public void testCase2(){
+        System.out.println("这是测试用例二");
+    }
+    @BeforeMethod
+    public void beforeMethod(){
+        System.out.println("beforeMethod这是在测试方法之前运行的");
+    }
+    @AfterMethod
+    public void afterMethod(){
+        System.out.println("aftreMethod这是在方法之后运行的");
     }
 }
